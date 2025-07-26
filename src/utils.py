b47fce78e2f461044cccdcde7436c297796f9928
vihaan2512 <malik.vihaan2512@gmail.com>
2025-07-26 13:53:54 +0530

    Final commit

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+import pickle
 
 def save_object(file_path,obj):
     try:
@@ -45,6 +46,14 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
